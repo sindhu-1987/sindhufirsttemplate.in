8d7d185612559e9e732bf6f92f32f34af88ff040
sindhu-1987 <87520240+sindhu-1987@users.noreply.github.com>
2021-08-14 17:13:52 +0530

    Add files via upload

---
+++ firsttemplate.js
@@ -0,0 +1,3 @@
+const searchIcon=document.querySelector(".search-icon");
+const searchContainer= document.querySelector(".search-container");
+searchIcon.addEventListener("click", function(){searchContainer.classList.toggle("active");})
