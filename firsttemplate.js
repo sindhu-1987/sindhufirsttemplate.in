9fd37683b99fe9ecfb2a831640ed42e2ae788791
sindhu-1987 <87520240+sindhu-1987@users.noreply.github.com>
2021-08-16 15:53:06 +0530

    Delete firsttemplate.js

--- firsttemplate.js
+++
@@ -1,3 +0,0 @@
-const searchIcon=document.querySelector(".search-icon");
-const searchContainer= document.querySelector(".search-container");
-searchIcon.addEventListener("click", function(){searchContainer.classList.toggle("active");})
